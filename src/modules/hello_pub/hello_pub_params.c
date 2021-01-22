7940556cf648c7dd556d604580a0f7849ac45ddf
tito97sp <tito97_sp@hotmail.com>
2021-01-23 00:49:20 +0100

    new update

---
+++ src/modules/hello_pub/hello_pub_params.c
@@ -0,0 +1,13 @@
+/**
+ * Hello param 
+ *
+ * Hello param for testing 
+ *
+ * @min 0
+ * @max 1
+ * @reboot_required true 
+ * @boolean 
+ * @group hello
+ */
+
+PARAM_DEFINE_INT32(HELLO_PARAM, 0);
