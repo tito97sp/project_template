c08359ca59d3dd0233e79aeaad1c22b1ebc7a34e
asanchez <asanchez@itainnova.es>
2021-05-31 12:44:26 +0200

    add folder for simulink models

---
+++ src/modules/simulink_models/pubsub_multitask/pubsub_multitask_nuttx/pubsub_multitask_types.h
@@ -0,0 +1,97 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: pubsub_multitask_types.h
+//
+// Code generated for Simulink model 'pubsub_multitask'.
+//
+// Model version                  : 1.5
+// Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+// C/C++ source code generated on : Sun May 30 22:17:46 2021
+//
+// Target selection: nuttx_ec.tlc
+// Embedded hardware selection: Intel->x86-64 (Windows64)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_pubsub_multitask_types_h_
+#define RTW_HEADER_pubsub_multitask_types_h_
+#include "rtwtypes.h"
+#include <uORB/topics/aa_tito_prueba.h>
+
+// Model Code Variants
+#ifndef struct_tag_hYhAI2VNT1j8Vnv0YZeb1D
+#define struct_tag_hYhAI2VNT1j8Vnv0YZeb1D
+
+struct tag_hYhAI2VNT1j8Vnv0YZeb1D
+{
+  boolean_T matlabCodegenIsDeleted;
+  int32_T isInitialized;
+  boolean_T isSetupComplete;
+  orb_advert_t orbAdvertiseObj;
+  orb_metadata_t * orbMetadataObj;
+};
+
+#endif                                 //struct_tag_hYhAI2VNT1j8Vnv0YZeb1D
+
+#ifndef typedef_nuttx_internal_block_Publisher_pubsub_multitask_T
+#define typedef_nuttx_internal_block_Publisher_pubsub_multitask_T
+
+typedef struct tag_hYhAI2VNT1j8Vnv0YZeb1D
+  nuttx_internal_block_Publisher_pubsub_multitask_T;
+
+#endif               //typedef_nuttx_internal_block_Publisher_pubsub_multitask_T
+
+#ifndef struct_tag_YgA2umAxY1QaUdNJg8JkbB
+#define struct_tag_YgA2umAxY1QaUdNJg8JkbB
+
+struct tag_YgA2umAxY1QaUdNJg8JkbB
+{
+  int32_T __dummy;
+};
+
+#endif                                 //struct_tag_YgA2umAxY1QaUdNJg8JkbB
+
+#ifndef typedef_b_px4_internal_block_SampleTimeImpl_pubsub_multitask_T
+#define typedef_b_px4_internal_block_SampleTimeImpl_pubsub_multitask_T
+
+typedef struct tag_YgA2umAxY1QaUdNJg8JkbB
+  b_px4_internal_block_SampleTimeImpl_pubsub_multitask_T;
+
+#endif          //typedef_b_px4_internal_block_SampleTimeImpl_pubsub_multitask_T
+
+#ifndef struct_tag_TU9Rnn74ZJKi9RaH5Ax4lF
+#define struct_tag_TU9Rnn74ZJKi9RaH5Ax4lF
+
+struct tag_TU9Rnn74ZJKi9RaH5Ax4lF
+{
+  boolean_T matlabCodegenIsDeleted;
+  int32_T isInitialized;
+  boolean_T isSetupComplete;
+  b_px4_internal_block_SampleTimeImpl_pubsub_multitask_T SampleTimeHandler;
+  pollfd_t eventStructObj;
+  orb_metadata_t * orbMetadataObj;
+};
+
+#endif                                 //struct_tag_TU9Rnn74ZJKi9RaH5Ax4lF
+
+#ifndef typedef_nuttx_internal_block_Subscriber_pubsub_multitask_T
+#define typedef_nuttx_internal_block_Subscriber_pubsub_multitask_T
+
+typedef struct tag_TU9Rnn74ZJKi9RaH5Ax4lF
+  nuttx_internal_block_Subscriber_pubsub_multitask_T;
+
+#endif              //typedef_nuttx_internal_block_Subscriber_pubsub_multitask_T
+
+// Forward declaration for rtModel
+typedef struct tag_RTM_pubsub_multitask_T RT_MODEL_pubsub_multitask_T;
+
+#endif                                 // RTW_HEADER_pubsub_multitask_types_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ src/modules/simulink_models/pubsub_multitask/pubsub_multitask_nuttx/rt_main.h
@@ -0,0 +1,60 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: rt_main.h
+//
+// Code generated for Simulink model 'pubsub_multitask'.
+//
+// Model version                  : 1.5
+// Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+// C/C++ source code generated on : Sun May 30 22:17:46 2021
+//
+// Target selection: nuttx_ec.tlc
+// Embedded hardware selection: Intel->x86-64 (Windows64)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_rt_main_h_
+#define RTW_HEADER_rt_main_h_
+#include <platform_common/module.h>
+#include <platform_common/log.h>
+
+extern "C" __EXPORT int_T pubsub_multitask_main(int_T argc, char_T *argv[]);
+class pubsub_multitask_module : public ModuleBase<pubsub_multitask_module>
+{
+ public:
+  pubsub_multitask_module()
+  {
+  }
+
+  virtual ~pubsub_multitask_module() = default;
+
+  //* @see ModuleBase
+  static int_T task_spawn(int_T argc, char_T *argv[]);
+
+  //* @see ModuleBase
+  static pubsub_multitask_module *instantiate(int_T argc, char_T *argv[]);
+
+  //* @see ModuleBase
+  static int_T custom_command(int_T argc, char_T *argv[]);
+
+  //* @see ModuleBase
+  static int_T print_usage(const char_T *reason = nullptr);
+
+  //* @see ModuleBase::run()
+  void run() override;
+
+  //* @see ModuleBase::print_status()
+  //int_T print_status() override;
+ private:
+};
+
+#endif                                 // RTW_HEADER_rt_main_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
